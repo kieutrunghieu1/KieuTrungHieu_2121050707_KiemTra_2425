4f6d12ee2e43a3392a94fae130bd19e71327df20
kieutrunghieu1@gmail.com <kieuhieu1001@gmail.com>
2025-05-21 00:05:29 +0700

    KiemtraB

---
+++ KieuTrungHieu_2121050707_KiemTra_2425/Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace KieuTrungHieu_2121050707_KiemTra_2425.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
